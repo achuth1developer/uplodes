120c7a0a370d43e645c82bfd8390984e0aaecb76
achuth1developer <35717718+achuth1developer@users.noreply.github.com>
2018-02-06 10:18:56 +0530

    Add files via upload

---
+++ all data types my activities.py
@@ -0,0 +1,13 @@
+mylist = [0,1, 2, 3, 4]
+mylist[0] = 'biscuits'
+mylist[1] = 'milk'
+mylist[2] = 'shirt and pant'
+mylist[3] = 'mobile'
+mylist[4] = '4\'o\'clock'
+a=('my daily activities, ill buy')
+b=('after buying buscuits.Iwill dip in')
+c=('and ill complete')
+d=('ill get ready to go to collage by wearing ')
+e=('ill use')
+f=('return home at')
+print(a,mylist[0],b,mylist[1],c,d,mylist[2],e,mylist[3],f,mylist[4])
